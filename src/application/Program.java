dd3b3c430c865779bd42987a84d1d3e203451edf
Thiago Antonio <thiagoapsx@gmail.com>
2024-05-01 11:59:16 -0300

    add Locale.setDefault

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,6 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Product;
@@ -15,7 +16,7 @@ import entities.Product;
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		List<Product> products = new ArrayList<>();
 		
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -49,6 +49,4 @@ public class Product {
 				+ ", "
 				+ String.format("%.2f", totalValue());
 	}
-	
-
 }

46cf78cf3ba4c9f4862c89daeb8fe7d7ba333e9b
Thiago Antonio <thiagoapsx@gmail.com>
2024-05-01 11:44:07 -0300

    Files exercise, before correction

---
+++ src/application/Program.java
@@ -0,0 +1,62 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		List<Product> products = new ArrayList<>();
+		
+		System.out.println("Enter the file path: ");
+		String path = sc.nextLine();
+		
+		File filePath = new File(path);
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
+			String line = br.readLine();
+			while(line != null) {
+				String[] fields = line.split(",");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				int quantity = Integer.parseInt(fields[2]);				
+				products.add(new Product(name, price, quantity));				
+				line = br.readLine();							
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		String newPath = "c:\\temp";
+		boolean success = new File(newPath + "\\out").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("c:\\temp\\out\\summary.csv"))){
+			
+			for (Product p : products) {
+				bw.write(p.getName() + ", " + p.totalValue());
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+				
+		System.out.println("PRODUCTS WITH TOTAL VALUE:");
+		for (Product p : products) {
+			System.out.println(p);
+		}
+				
+		sc.close();
+	}
+}
